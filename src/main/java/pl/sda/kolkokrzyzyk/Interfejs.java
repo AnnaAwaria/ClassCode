fdb9d790595ee39d87321127fd058538e56dbb65
Anna Iga Frąckowiak <retrorain@gmail.com>
2017-01-12 13:44:31 +0100

    Revert "Merge remote-tracking branch 'origin/kolkoikrzyzyk'"
    This reverts commit d47696bfb62c208192ad962ce1d50a520ba5a6b9, reversing
    changes made to 040b6ea67d28dd41ae2957dca8b80dd88dd8f649.

--- src/main/java/pl/sda/kolkokrzyzyk/Interfejs.java
+++ src/main/java/pl/sda/kolkokrzyzyk/Interfejs.java
@@ -32,15 +32,4 @@ public class Interfejs {
 		return pozycja;
 
 	}
-
-	public void pokazPlansze(Plansza plansza) {
-		System.out.println(plansza.toString());
-	}
-
-	public void podanoNiepoprawnePole() {
-		System.out.println("Podano niepoprawne pole");
-		Scanner scanner = new Scanner(System.in);
-		scanner.nextLine();
-	}
-
 }
--- src/main/java/pl/sda/kolkokrzyzyk/Main.java
+++ src/main/java/pl/sda/kolkokrzyzyk/Main.java
@@ -1,8 +1,5 @@
 package pl.sda.kolkokrzyzyk;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,44 +7,18 @@ public class Main {
 		Interfejs interfejs = new Interfejs();
 		Plansza plansza = new Plansza();
 		int menu = interfejs.menu();
-
-		Map<String, String> mapaGraczy = new HashMap<>();
 		String gracz1 = interfejs.podajImie();
 		String gracz2 = interfejs.podajImie();
-		mapaGraczy.put(gracz1, "X");
-		mapaGraczy.put(gracz2, "O");
 
-		for (int i = 0; i < 9; i++) {
+		int pozycja = interfejs.podajPole(gracz1);
+		boolean czyWstawiono = plansza.wstawZnak("X", pozycja);
+		czyWstawiono = plansza.wstawZnak("O", pozycja);
 
-			interfejs.pokazPlansze(plansza);
-			boolean czyWstawiono;
-			String aktualnyGracz = null;
-			String wstawianyZnak = null;
-			if (i % 2 == 0) {
-				aktualnyGracz = gracz1;
-			} else {
-				aktualnyGracz = gracz2;
-			}
-
-			wstawianyZnak = mapaGraczy.get(aktualnyGracz);
-
-			do {
-				int pozycja = interfejs.podajPole(aktualnyGracz);
-				czyWstawiono = plansza.wstawZnak(wstawianyZnak, pozycja);
-				if (!czyWstawiono) {
-					interfejs.podanoNiepoprawnePole();
-				}
-
-			} while (!czyWstawiono);
-
-		}
-		interfejs.pokazPlansze(plansza);
-
-		/*
-		 * System.out.println(" |  | "); System.out.println(" ----- ");
-		 * System.out.println(" |  | "); System.out.println(" ----- ");
-		 * System.out.println(" |  | ");
-		 */
+		System.out.println(" |  | ");
+		System.out.println(" ----- ");
+		System.out.println(" |  | ");
+		System.out.println(" ----- ");
+		System.out.println(" |  | ");
 
 	}
 
